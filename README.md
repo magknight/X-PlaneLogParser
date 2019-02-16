378bd97b49e0edaba3bed00ebfdcdcb0b55cb3d7
Alex Beavil <alex@aeroniemi.ovh>
2019-02-16 20:11:03 +0000

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # X-PlaneLogParser
+As the name suggests, it's a parser for the X-Plane log file. I was getting really annoyed with having to look through people's log files when they were immensely log, and contained 90% stuff I didn't care about. Here, my solution is to remove the lines that are not of interest.
+
+It's not very exciting
---
+++ index.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+const args = process.argv.slice(2)
+var path = args['path']
+if (path == null) {
+    path = "./Log.txt"
+}
+var output = fs.createWriteStream('formattedLog.txt');
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(path)
+});
+var tableOfIgnores = [
+    'XSB:',
+    'XSB WARNING: ',
+    '            ',
+    "Custom Scenery",
+    "Found an illegal name",
+    "We found a duplicate runway",
+    "in sign",
+    "Could not find tile ",
+    "Resources/default scenery",
+    "GroundTraffic:"
+]
+var outputTable = []
+
+function after_forloop() {
+    // task you want to do after for loop finishes it's execution
+}
+
+lineReader.on('line', function (line) {
+
+    for (i = 0; i < tableOfIgnores.length; i++) {
+        let act = true
+        if (line.match(tableOfIgnores[i])) {
+            // console.log("invalid line: " + i)
+            break
+        } else if (line === "") { break }
+        if (i === tableOfIgnores.length - 1) {
+            outputTable.push(line)
+        }
+    }
+});
+lineReader.on("close", function () {
+    output.on('error', function (err) { /* error handling */ });
+    outputTable.forEach(function (v) { output.write(v + '\n'); });
+    output.end();
+    console.log("Done")
+}
+)
+
+
