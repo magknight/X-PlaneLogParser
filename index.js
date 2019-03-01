5781bef6e86de671552e2b84469ae5ee08dd24ae
Alex Beavil <alex@aeroniemi.ovh>
2019-03-01 15:07:00 +0000

    Added additional excludes; made it start vscode with the output file when done

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 var fs = require('fs');
 const args = process.argv.slice(2)
+var exec = require('child_process').exec, child;
 var path = args['path']
 if (path == null) {
     path = "./Log.txt"
 }
-var output = fs.createWriteStream('formattedLog.txt');
+var output = fs.createWriteStream(path + ".min");
 var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream(path)
 });
+
+
+
 var tableOfIgnores = [
     'XSB:',
     'XSB WARNING: ',
@@ -19,7 +23,11 @@ var tableOfIgnores = [
     "in sign",
     "Could not find tile ",
     "Resources/default scenery",
-    "GroundTraffic:"
+    "GroundTraffic:",
+    "Xchecklist",
+    "Terrain radar plugin",
+    "Airport-Navigator",
+    "FlyWithLua: Loaded sound file"
 ]
 var outputTable = []
 
@@ -42,6 +50,15 @@ lineReader.on("close", function () {
     outputTable.forEach(function (v) { output.write(v + '\n'); });
     output.end();
     console.log("Done")
+    exec('code ' + path + ".min",
+        function (error, stdout, stderr) {
+
+            if (error !== null) {
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                console.log('exec error: ' + error);
+            }
+        });
 }
 )
 
